cdaabfb9f6b1213b4eb719e5be7f924363a4e725
Edward Wang <edward.wang314@gmail.com>
2020-12-26 15:08:23 -0500

    solve lint 304

--- Lintcode Java/304. Maximum Product/src/Solution.java
+++ Lintcode Java/304. Maximum Product/src/Solution.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +10,6 @@ public class Solution {
      */
     public long MaximumProduct(int[] nums) {
         // write your code here
-        long res = 0;
-        
         int l = 0, r = nums.length - 1, pos = 0;
         while ((pos = partition(nums, l, r)) != nums.length - 3) {
             if (pos < nums.length - 3) {
@@ -22,43 +21,59 @@ public class Solution {
         l = 0;
         r = nums.length - 1;
         while ((pos = partition(nums, l, r)) != 1) {
-            if (pos < nums.length - 3) {
+            if (pos < 1) {
                 l = pos + 1;
             } else {
                 r = pos - 1;
             }
         }
-    
-        List<Integer> list = new ArrayList<>();
+        
+        List<Long> list = new ArrayList<>();
         int i = 0;
         for (i = nums.length - 1; i >= nums.length - 3; i--) {
-            list.add(nums[i]);
+            list.add((long) nums[i]);
         }
-    
-        for (int j = 0; j < list.size(); j++) {
         
+        for (int j = 0; j <= Math.min(1, i); j++) {
+            list.add((long) nums[j]);
         }
+        
+        long res = Long.MIN_VALUE;
+        for (int j = 0; j < list.size() - 2; j++) {
+            for (int k = j + 1; k < list.size() - 1; k++) {
+                for (int m = k + 1; m < list.size(); m++) {
+                    res = Math.max(res, list.get(j) * list.get(k) * list.get(m));
+                }
+            }
+        }
+        
         return res;
     }
     
-    private int partition(int[] nums, int i, int j) {
-        int pivot = nums[i];
-        while (i < j) {
-            while (i < j && nums[i] <= nums[j]) {
-                j--;
-            }
-            nums[i] = nums[j];
-            while (i < j && nums[i] <= nums[j]) {
-                i++;
-            }
-            nums[j] = nums[i];
+    private int partition(int[] nums, int l, int r) {
+        if (l >= r) {
+            return l;
         }
         
-        nums[i] = pivot;
-        return i;
+        int pivot = nums[l];
+        while (l < r) {
+            while (l < r && pivot <= nums[r]) {
+                r--;
+            }
+            nums[l] = nums[r];
+            while (l < r && nums[l] <= pivot) {
+                l++;
+            }
+            nums[r] = nums[l];
+        }
+        
+        nums[l] = pivot;
+        return l;
     }
     
     public static void main(String[] args) {
-        System.out.println(new Solution().MaximumProduct(new int[]{3, 4, 1, 2}));
+        // System.out.println(new Solution().MaximumProduct(new int[]{3, 4, 1, 2}));
+        // System.out.println(new Solution().MaximumProduct(new int[]{-20, -10, -15, 5, -5, -18, -9, -2, 14, -15}));
+        System.out.println(new Solution().MaximumProduct(new int[]{2, -6, -5, 5, -15, 19, -19, -16, 5, -7}));
     }
 }
