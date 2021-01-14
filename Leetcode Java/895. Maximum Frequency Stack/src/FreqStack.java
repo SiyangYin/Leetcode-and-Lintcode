7d2b720b1d16c44343dc6e78f0d3e5d9a03758a3
edwardwang618 <edward.wang618@gmail.com>
2021-01-14 00:05:26 -0600

    solved leet 895

---
+++ Leetcode Java/895. Maximum Frequency Stack/src/FreqStack.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class FreqStack {
+    
+    private Map<Integer, Integer> freq;
+    private List<Deque<Integer>> list;
+    
+    public FreqStack() {
+        freq = new HashMap<>();
+        list = new ArrayList<>();
+    }
+    
+    public void push(int x) {
+        freq.put(x, freq.getOrDefault(x, 0) + 1);
+        if (list.size() < freq.get(x)) {
+            list.add(new ArrayDeque<>());
+        }
+        
+        list.get(freq.get(x) - 1).push(x);
+    }
+    
+    public int pop() {
+        while (!list.isEmpty() && list.get(list.size() - 1).isEmpty()) {
+            list.remove(list.size() - 1);
+        }
+        
+        int res = list.get(list.size() - 1).pop();
+        freq.put(res, freq.get(res) - 1);
+        return res;
+    }
+}
