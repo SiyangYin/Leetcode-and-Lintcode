805187633042d7120849c4f924a0d783b9ce6493
edwardwang618 <edward.wang618@gmail.com>
2021-01-26 19:31:47 -0600

    solved leet 10

--- Leetcode Java/10. Regular Expression Matching/src/Solution.java
+++ Leetcode Java/10. Regular Expression Matching/src/Solution.java
@@ -1,5 +1,23 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-    
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (j < p.length() && p.charAt(j) == '*') {
+                    continue;
+                }
+                
+                if (i >= 1 && p.charAt(j - 1) != '*') {
+                    dp[i][j] = dp[i - 1][j - 1] && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1));
+                }
+                
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (i > 0 && dp[i - 1][j] && (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.'));
+                }
+            }
+        }
+        
+        return dp[s.length()][p.length()];
     }
 }
