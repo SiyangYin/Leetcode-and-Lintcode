bdd59cb3cd0a6c63c2f4026ffe7b8b2f8f7cadc8
edwardwang618 <edward.wang618@gmail.com>
2021-01-28 16:56:06 -0600

    solved lint 499

---
+++ Lintcode Java/499. Word Count (Map Reduce)/src/WordCount.java
@@ -0,0 +1,37 @@
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class WordCount {
+    public static class Map {
+        public void map(String key, String value, OutputCollector<String, Integer> output) {
+            // Write your code here
+            // Output the results into output buffer.
+            // Ps. output.collect(String key, int value);
+            StringTokenizer tokenizer = new StringTokenizer(value);
+            while (tokenizer.hasMoreTokens()) {
+                String word = tokenizer.nextToken();
+                output.collect(word, 1);
+            }
+        }
+    }
+    
+    public static class Reduce {
+        public void reduce(String key, Iterator<Integer> values,
+                           OutputCollector<String, Integer> output) {
+            // Write your code here
+            // Output the results into output buffer.
+            // Ps. output.collect(String key, int value);
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next();
+            }
+            output.collect(key, sum);
+        }
+    }
+}
+
+
+class OutputCollector<K, V> {
+    // Adds a key/value pair to the output buffer
+    public void collect(K key, V value){};
+}
