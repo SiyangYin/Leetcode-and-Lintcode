e21646a4ad83e0384365219c7df4dc5a9af22805
edwardwang618 <edward.wang618@gmail.com>
2021-01-30 00:54:47 -0600

    solved lint 1481

--- Lintcode Java/1481. Unique Substring/src/Solution.java
+++ Lintcode Java/1481. Unique Substring/src/Solution.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Solution {
     /**
@@ -10,13 +12,34 @@ public class Solution {
     public List<String> uniqueSubstring(String s, int k) {
         // Write your code here
         List<String> res = new ArrayList<>();
+        if (s.length() < k) {
+            return res;
+        }
         
-        long MOD =
+        long p = 131, hash = 0, pow = 1;
+        Set<Long> set = new HashSet<>();
+        for (int i = 0; i < k; i++) {
+            hash = hash * p + s.charAt(i);
+            pow *= p;
+        }
+        set.add(hash);
+        res.add(s.substring(0, k));
         
+        for (int i = k; i < s.length(); i++) {
+            hash = hash * p + s.charAt(i);
+            hash -= s.charAt(i - k) * pow;
+            
+            if (!set.contains(hash)) {
+                set.add(hash);
+                res.add(s.substring(i - k + 1, i + 1));
+            }
+        }
+    
+        res.sort(null);
         return res;
     }
     
     public static void main(String[] args) {
-        System.out.println(Integer.valueOf(119999999).hashCode());
+        System.out.println(new Solution().uniqueSubstring("caaab", 2));
     }
 }
