2bcc39db2b6c1d451ea3c64be61ba3359dcee413
edwardwang618 <edward.wang618@gmail.com>
2021-02-03 10:38:48 -0600

    solved leet 685

--- Leetcode Java/685. Redundant Connection II/src/Solution.java
+++ Leetcode Java/685. Redundant Connection II/src/Solution.java
@@ -1,4 +1,71 @@
+import java.util.Arrays;
+
 public class Solution {
-
-
+    
+    class UnionFind {
+        
+        private int[] parent;
+        
+        public UnionFind(int size) {
+            parent = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+            }
+        }
+        
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            
+            return parent[x];
+        }
+        
+        public boolean union(int x, int y) {
+            int px = find(x), py = find(y);
+            if (px == py) {
+                return false;
+            }
+            
+            parent[px] = py;
+            return true;
+        }
+    }
+    
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        int n = edges.length;
+        
+        int[] father = new int[n + 1];
+        Arrays.fill(father, -1);
+        int[][] res = null;
+        for (int[] edge : edges) {
+            int from = edge[0], to = edge[1];
+            if (father[to] != -1) {
+                res = new int[][]{{father[to], to}, {from, to}};
+                edge[0] = edge[1] = -1;
+                break;
+            }
+            
+            father[to] = from;
+        }
+        
+        UnionFind uf = new UnionFind(n + 1);
+        for (int[] edge : edges) {
+            int from = edge[0], to = edge[1];
+            if (from == -1 || to == -1) {
+                continue;
+            }
+            
+            if (!uf.union(from, to)) {
+                return res == null ? edge : res[0];
+            }
+        }
+        
+        return res[1];
+    }
+    
+    public static void main(String[] args) {
+        int[][] e = {{2, 1}, {3, 1}, {4, 2}, {1, 4}};
+        System.out.println(Arrays.toString(new Solution().findRedundantDirectedConnection(e)));
+    }
 }
