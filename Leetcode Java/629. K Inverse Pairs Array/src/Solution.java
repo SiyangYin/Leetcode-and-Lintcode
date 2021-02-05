3982cf9ff823eec3e248de03e9f3f72d0cdce4a4
edwardwang618 <edward.wang618@gmail.com>
2021-02-05 16:44:00 -0600

    solved leet 629

--- Leetcode Java/629. K Inverse Pairs Array/src/Solution.java
+++ Leetcode Java/629. K Inverse Pairs Array/src/Solution.java
@@ -1,9 +1,24 @@
 public class Solution {
     public int kInversePairs(int n, int k) {
-        int MOD = (int) (1E9+7);
+        int MOD = (int) (1E9 + 7);
         int[][] dp = new int[n + 1][k + 1];
+        dp[1][0] = 1;
         
+        for (int i = 2; i <= n; i++) {
+            long sum = 0;
+            for (int j = 0; j <= k; j++) {
+                sum += dp[i - 1][j];
+                if (j >= i) {
+                    sum -= dp[i - 1][j - i];
+                }
+                dp[i][j] = (int) (sum % MOD);
+            }
+        }
         
-        
+        return (dp[n][k] + MOD) % MOD;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new Solution().kInversePairs(3, 1));
     }
 }
