483f87de24d9c5e9eea930698396a1e89623294d
edwardwang618 <edward.wang618@gmail.com>
2021-02-07 16:15:18 -0600

    solved leet 632

--- Leetcode Java/630. Course Schedule III/src/Solution.java
+++ Leetcode Java/630. Course Schedule III/src/Solution.java
@@ -4,14 +4,14 @@ import java.util.PriorityQueue;
 public class Solution {
     public int scheduleCourse(int[][] courses) {
         Arrays.sort(courses, (c1, c2) -> Integer.compare(c1[1], c2[1]));
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((c1, c2) -> -Integer.compare(c1[0], c2[0]));
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((c1, c2) -> -Integer.compare(c1, c2));
     
         int total = 0;
         for (int[] course : courses) {
             total += course[0];
-            maxHeap.offer(course);
+            maxHeap.offer(course[0]);
             if (total > course[1]) {
-                total -= maxHeap.poll()[0];
+                total -= maxHeap.poll();
             }
         }
         
---
+++ Leetcode Java/632. Smallest Range Covering Elements from K Lists/src/Solution.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p1[0], p2[0]));
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.size(); i++) {
+            int val = nums.get(i).get(0);
+            maxHeap.offer(new int[]{val, i, 0});
+            max = Math.max(max, val);
+        }
+        
+        int[] res = null;
+        while (!maxHeap.isEmpty()) {
+            int[] top = maxHeap.poll();
+            int l = top[0], r = max;
+            if (res == null || res[1] - res[0] > r - l || (res[1] - res[0] == r - l && l < res[0])) {
+                res = new int[]{l, r};
+            }
+            
+            int row = top[1], idx = top[2];
+            if (idx + 1 < nums.get(row).size()) {
+                int val = nums.get(row).get(idx + 1);
+                maxHeap.offer(new int[]{val, row, idx + 1});
+                max = Math.max(max, val);
+            } else {
+                break;
+            }
+        }
+        
+        return res;
+    }
+}
