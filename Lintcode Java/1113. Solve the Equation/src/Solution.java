cc6e223a96e5c0fc1f272a9d6c364738f74c11d4
edwardwang618 <edward.wang618@gmail.com>
2021-02-07 23:47:50 -0600

    solved lint 1113 1114

--- Lintcode Java/1113. Solve the Equation/src/Solution.java
+++ Lintcode Java/1113. Solve the Equation/src/Solution.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Solution {
     /**
      * @param equation: a string
@@ -8,71 +6,65 @@ public class Solution {
     public String solveEquation(String equation) {
         // write your code here
         String[] s = equation.split("=");
-        String left = s[0];
-        String right = s[1];
-        int[] leftPair = parseExpression(left);
-        int[] rightPair = parseExpression(right);
+        int[] leftPair = parseExpression(s[0]), rightPair = parseExpression(s[1]);
         int[] finalPair = new int[]{leftPair[0] - rightPair[0], leftPair[1] - rightPair[1]};
         if (finalPair[0] == 0) {
-            if (finalPair[1] == 0) {
-                return "Infinite solutions";
-            } else {
-                return "No solution";
-            }
+            return finalPair[1] == 0 ? "Infinite solutions" : "No solution";
         }
+        
         return "x=" + (-(finalPair[1] / finalPair[0]));
     }
     
     //  return the coefficient of x and constant
     private int[] parseExpression(String expression) {
-        int coefficient = 0;
-        int constant = 0;
-        String term = "";
+        int coefficient = 0, constant = 0;
+        StringBuilder sb = new StringBuilder();
         if (!expression.startsWith("+") && !expression.startsWith("-")) {
             expression = "+" + expression;
         }
-        term += expression.charAt(0);
+        
+        sb.append(expression.charAt(0));
         for (int i = 1; i < expression.length(); i++) {
             if (expression.charAt(i) != '+' && expression.charAt(i) != '-') {
-                term += expression.charAt(i);
+                sb.append(expression.charAt(i));
             }
-            if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || i == expression.length() - 1){
-                int[] pair = parseTerm(term);
+            if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || i == expression.length() - 1) {
+                int[] pair = parseTerm(sb.toString());
                 coefficient += pair[0];
                 constant += pair[1];
-                term = "" + expression.charAt(i);
+                sb = new StringBuilder(String.valueOf(expression.charAt(i)));
             }
         }
+        
         return new int[]{coefficient, constant};
     }
     
     //  return the coefficient and constant pair
     private int[] parseTerm(String term) {
         int[] pair = new int[2];
-        if (term.equals("+x")) {
-            pair[0] = 1; pair[1] = 0;
-            return pair;
-        }
-        if (term.equals("-x")) {
-            pair[0] = -1; pair[1] = 0;
+        if ("+x".equals(term) || "-x".equals(term)) {
+            pair[0] = term.charAt(0) == '+' ? 1 : -1;
+            pair[1] = 0;
             return pair;
         }
         
         if (term.endsWith("x")) {
-            pair[0] = Integer.parseInt(term.substring(1, term.length()  - 1));
+            pair[0] = Integer.parseInt(term.substring(1, term.length() - 1));
             if (term.startsWith("-")) {
                 pair[0] = -pair[0];
             }
         } else {
-            pair[1] = Integer.parseInt(term.substring(1, term.length()));
+            pair[1] = Integer.parseInt(term.substring(1));
             if (term.startsWith("-")) {
                 pair[1] = -pair[1];
             }
         }
+        
         return pair;
     }
     
     public static void main(String[] args) {
-        System.out.println(new Solution().solveEquation("-x+x+3x=2x-x+x"));
+        // System.out.println(new Solution().solveEquation("-x+x+3x=2x-x+x"));
+        System.out.println(new Solution().solveEquation("2x=x"));
     }
 }
--- Lintcode Java/1114. Shopping Offers/src/Solution.java
+++ Lintcode Java/1114. Shopping Offers/src/Solution.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 
 public class Solution {
     /**
@@ -16,22 +14,17 @@ public class Solution {
         for (int i = 0; i < price.size(); i++) {
             result += price.get(i) * needs.get(i);
         }
+        
         for (int i = 0; i < special.size(); i++) {
             List<Integer> newNeeds = canUseSpecial(special.get(i), needs);
             if (newNeeds != null) {
                 result =  Math.min(result, special.get(i).get(needs.size()) + shoppingOffers(price, special, newNeeds));
             }
         }
+        
         return result;
     }
     
-    
-    /**
-     *
-     * @param specialOffer
-     * @param needs
-     * @return newNeeds if can use special order, null if otherwise
-     */
     private List<Integer> canUseSpecial(List<Integer> specialOffer, List<Integer> needs) {
         List<Integer> newNeeds = new ArrayList<>();
         for (int i = 0; i < needs.size(); i++) {
@@ -41,16 +34,7 @@ public class Solution {
                 newNeeds.add(needs.get(i) - specialOffer.get(i));
             }
         }
-        return newNeeds;
-    }
-    
-    public static void main(String[] args) {
-        List<Integer> price = Arrays.asList(2, 3, 4);
-        List<List<Integer>> special = new ArrayList<>();
-        special.add(Arrays.asList(1, 1, 0, 4));
-        special.add(Arrays.asList(2, 2, 1, 9));
-        List<Integer> needs = Arrays.asList(1, 2, 1);
-        System.out.println(new Solution().shoppingOffers(price, special, needs));
         
+        return newNeeds;
     }
 }
