b82ba8afeaae1fe003b1e5995a124f5103e456fc
edwardwang618 <edward.wang618@gmail.com>
2021-02-13 21:16:05 -0600

    solved lint 1227

--- Lintcode Java/1227. Repeated Substring Pattern/src/Solution.java
+++ Lintcode Java/1227. Repeated Substring Pattern/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Solution {
     /**
      * @param s: a string
@@ -5,6 +7,27 @@ public class Solution {
      */
     public boolean repeatedSubstringPattern(String s) {
         // write your code here
-        return false;
+        int[] next = buildNext(s + '#');
+        System.out.println(Arrays.toString(next));
+        int len = s.length() - next[s.length()];
+        return len != s.length() && s.length() % len == 0;
+    }
+    
+    private int[] buildNext(String s) {
+        int[] next = new int[s.length()];
+        next[0] = -1;
+        for (int i = 0, j = -1; i < s.length() - 1; ) {
+            if (j < 0 || s.charAt(i) == s.charAt(j)) {
+                next[++i] = ++j;
+            } else {
+                j = next[j];
+            }
+        }
+        
+        return next;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new Solution().repeatedSubstringPattern("abab"));
     }
 }
