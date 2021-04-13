90dbf0bcabe556d578e81a2ace9a60c1aa535ed3
edwardwang618 <edward.wang618@gmail.com>
2021-04-12 21:07:27 -0400

    leet 1368

---
+++ Leetcode Java/1368. Minimum Cost to Make at Least One Valid Path in a Grid/src/Solution.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Solution {
+    public int minCost(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] d = {1, 0, -1, 0, 1};
+        Map<Integer, Integer> mapToD = Map.of(
+                0, 3,
+                1, 2,
+                2, 4,
+                3, 1);
+        
+        Deque<Integer> deque = new ArrayDeque<>();
+        int[][] dist = new int[m][n];
+        for (int[] row : dist) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        boolean[][] visited = new boolean[m][n];
+        int end = m * n - 1;
+        
+        deque.offerLast(0);
+        dist[0][0] = 0;
+        while (!deque.isEmpty()) {
+            int cur = deque.pollFirst();
+            if (visited[cur / n][cur % n]) {
+                continue;
+            }
+            
+            int x = cur / n, y = cur % n;
+            if (x == m - 1 && y == n - 1) {
+                return dist[m - 1][n - 1];
+            }
+            
+            visited[x][y] = true;
+            for (int i = 0; i < 4; i++) {
+                int nextX = x + d[i], nextY = y + d[i + 1];
+                if (!(0 <= nextX && nextX < m && 0 <= nextY && nextY < n)) {
+                    continue;
+                }
+                
+                int e = grid[x][y] == mapToD.get(i) ? 0 : 1;
+                if (!visited[nextX][nextY] && dist[nextX][nextY] > dist[x][y] + e) {
+                    dist[nextX][nextY] = dist[x][y] + e;
+                    if (e == 0) {
+                        deque.offerFirst(nextX * n + nextY);
+                    } else {
+                        deque.offerLast(nextX * n + nextY);
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int[][] g = {{1, 2}, {4, 3}};
+        System.out.println(new Solution().minCost(g));
+    }
+}
