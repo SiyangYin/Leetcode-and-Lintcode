c546b5b0e5f237e2f09c2dc04679e3bff7a0901e
edwardwang618 <edward.wang618@gmail.com>
2021-04-22 21:04:08 -0400

    leet 1822

---
+++ Leetcode Java/1822. Sign of the Product of an Array/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int arraySign(int[] nums) {
+        int res = 1;
+        for (int x : nums) {
+            if (x == 0) {
+                return 0;
+            } else if (x < 0) {
+                res *= -1;
+            }
+        }
+        
+        return res;
+    }
+}
