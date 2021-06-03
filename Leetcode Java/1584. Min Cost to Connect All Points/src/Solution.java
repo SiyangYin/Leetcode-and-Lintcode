467a8c1576fa2ed2da96d7aa10e2750b6d155a00
edwardwang618 <edward.wang618@gmail.com>
2021-06-03 09:33:39 -0400

    leet 1584

---
+++ Leetcode Java/1584. Min Cost to Connect All Points/src/Solution.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    
+    class UnionFind {
+        private int[] p;
+        public UnionFind(int size) {
+            p = new int[size];
+            for (int i = 0; i < p.length; i++) {
+                p[i] = i;
+            }
+        }
+        
+        public int find(int x) {
+            if (p[x] != x) {
+                p[x] = find(p[x]);
+            }
+            
+            return p[x];
+        }
+        
+        public boolean union(int x, int y) {
+            int px = find(x), py = find(y);
+            if (px != py) {
+                p[px] = py;
+                return true;
+            }
+            
+            return false;
+        }
+    }
+    
+    public int minCostConnectPoints(int[][] points) {
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                int[] x = points[i], y = points[j];
+                list.add(new int[]{i, j, Math.abs(x[0] - y[0]) + Math.abs(x[1] - y[1])});
+            }
+        }
+        
+        list.sort((p1, p2) -> Integer.compare(p1[2], p2[2]));
+        UnionFind uf = new UnionFind(points.length);
+        int res = 0;
+        for (int[] p : list) {
+            int x = p[0], y = p[1];
+            if (uf.union(x, y)) {
+                res += p[2];
+            }
+        }
+        
+        return res;
+    }
+}
