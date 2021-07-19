73747804e18e900cdc687d6fc7623830ca816a25
edwardwang618 <edward.wang618@gmail.com>
2021-07-19 16:47:49 -0400

    lint 1077

--- Lintcode Java/1077. Falling Squares/src/Solution.java
+++ Lintcode Java/1077. Falling Squares/src/Solution.java
@@ -4,9 +4,8 @@ import java.util.List;
 public class Solution {
     
     class SegTree {
-        
         class Node {
-            int l, r, v, lazy;
+            int l, r, M, lazy;
             
             public Node(int l, int r) {
                 this.l = l;
@@ -21,89 +20,89 @@ public class Solution {
             build(1, 0, size - 1);
         }
         
-        private void pushup(int u) {
-            tr[u].v = Math.max(tr[u << 1].v, tr[u << 1 | 1].v);
-        }
-        
-        private void pushdown(int u) {
-            int c = tr[u].lazy;
-            if (c != 0) {
-                tr[u].lazy = 0;
-                tr[u << 1].v = tr[u << 1 | 1].v = c;
-                tr[u << 1].lazy = tr[u << 1 | 1].lazy = c;
-            }
-        }
-        
         public void build(int u, int l, int r) {
             tr[u] = new Node(l, r);
             if (l == r) {
                 return;
             }
             
-            int mid = l + r >> 1;
-            build(u << 1, l, mid);
-            build(u << 1 | 1, mid + 1, r);
-        }
-        
-        public void update(int u, int l, int r, int c) {
-            if (l <= tr[u].l && tr[u].r <= r) {
-                tr[u].v = tr[u].lazy = c;
-                return;
-            }
-            
-            pushdown(u);
-            int mid = tr[u].l + tr[u].r >> 1;
-            if (l <= mid) {
-                update(u << 1, l, r, c);
-            }
-            if (mid + 1 <= r) {
-                update(u << 1 | 1, l, r, c);
-            }
-            pushup(u);
+            int m = l + (r - l >> 1);
+            build(u << 1, l, m);
+            build(u << 1 | 1, m + 1, r);
         }
         
         public int query(int u, int l, int r) {
             if (l <= tr[u].l && tr[u].r <= r) {
-                return tr[u].v;
+                return tr[u].M;
             }
             
             pushdown(u);
-            int res = 0, mid = tr[u].l + tr[u].r >> 1;
-            if (l <= mid) {
+            int res = 0, m = tr[u].l + (tr[u].r - tr[u].l >> 1);
+            if (l <= m) {
                 res = Math.max(res, query(u << 1, l, r));
             }
-            if (mid + 1 <= r) {
+            if (m + 1 <= r) {
                 res = Math.max(res, query(u << 1 | 1, l, r));
             }
             
             return res;
         }
         
-        public int query() {
-            return tr[1].v;
+        public void update(int u, int l, int r, int v) {
+            if (l <= tr[u].l && tr[u].r <= r) {
+                tr[u].M = tr[u].lazy = v;
+                return;
+            }
+            
+            pushdown(u);
+            int m = tr[u].l + (tr[u].r - tr[u].l >> 1);
+            if (l <= m) {
+                update(u << 1, l, r, v);
+            }
+            if (m + 1 <= r) {
+                update(u << 1 | 1, l, r, v);
+            }
+            pushup(u);
+        }
+        
+        private void pushup(int u) {
+            tr[u].M = Math.max(tr[u << 1].M, tr[u << 1 | 1].M);
+        }
+        
+        private void pushdown(int u) {
+            if (tr[u].lazy != 0) {
+                tr[u << 1].lazy = tr[u << 1 | 1].lazy = tr[u].lazy;
+                tr[u << 1].M = tr[u << 1 | 1].M = tr[u].lazy;
+                tr[u].lazy = 0;
+            }
         }
     }
     
+    /**
+     * @param positions: a list of (left, side_length)
+     * @return: return a list of integer
+     */
     public List<Integer> fallingSquares(int[][] positions) {
+        // write your code here
         List<Integer> xs = new ArrayList<>();
         for (int[] p : positions) {
-            int a = p[0], b = a + p[1];
+            int a = p[0], b = p[0] + p[1];
             xs.add(a * 2);
             xs.add(b * 2);
             xs.add(a + b);
         }
-        xs = unique(xs);
         
+        xs = unique(xs);
         SegTree segTree = new SegTree(xs.size());
         
         List<Integer> res = new ArrayList<>();
         for (int[] p : positions) {
-            int a = p[0], b = a + p[1];
+            int a = p[0], b = p[0] + p[1];
             a = get(a * 2, xs);
             b = get(b * 2, xs);
             int h = segTree.query(1, a + 1, b - 1);
             segTree.update(1, a, b, h + p[1]);
-            res.add(segTree.query());
+            res.add(segTree.query(1, 0, xs.size() - 1));
         }
         
         return res;
@@ -112,7 +111,7 @@ public class Solution {
     private int get(int x, List<Integer> xs) {
         int l = 0, r = xs.size() - 1;
         while (l < r) {
-            int m = l + r >> 1;
+            int m = l + (r - l >> 1);
             if (xs.get(m) >= x) {
                 r = m;
             } else {
@@ -123,16 +122,16 @@ public class Solution {
         return l;
     }
     
-    private List<Integer> unique(List<Integer> list) {
-        list.sort(Integer::compareTo);
+    private List<Integer> unique(List<Integer> xs) {
+        xs.sort(Integer::compareTo);
         int j = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (i == 0 || list.get(j - 1) != list.get(i)) {
-                list.set(j++, list.get(i));
+        for (int i = 0; i < xs.size(); i++) {
+            if (j == 0 || xs.get(j - 1) != xs.get(i)) {
+                xs.set(j++, xs.get(i));
             }
         }
         
-        return list.subList(0, j);
+        return xs.subList(0, j);
     }
     
     public static void main(String[] args) {
