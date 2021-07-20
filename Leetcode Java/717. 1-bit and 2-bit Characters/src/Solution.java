ab8b15e6ca95a8b04cb9de0fefc1faae7f39dbc8
edwardwang618 <edward.wang618@gmail.com>
2021-07-20 12:24:31 -0400

    leet 717

---
+++ Leetcode Java/717. 1-bit and 2-bit Characters/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        for (int i = 0; i < bits.length; i++) {
+            if (i == bits.length - 1 && bits[i] == 0) {
+                return true;
+            }
+            
+            if (bits[i] == 1) {
+                i++;
+            }
+        }
+        
+        return false;
+    }
+}
