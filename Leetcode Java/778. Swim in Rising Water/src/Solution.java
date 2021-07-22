628da5bb6de743cec5223eb9d58de549abf63864
edwardwang618 <edward.wang618@gmail.com>
2021-07-22 02:28:59 -0400

    leet 778

---
+++ Leetcode Java/778. Swim in Rising Water/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int swimInWater(int[][] grid) {
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p1[0], p2[0]));
+        int[] d = {-1, 0, 1, 0, -1};
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        minHeap.offer(new int[]{grid[0][0], 0, 0});
+        vis[0][0] = true;
+        while (!minHeap.isEmpty()) {
+            int[] cur = minHeap.poll();
+            int x = cur[1], y = cur[2];
+            if (x == grid.length - 1 && y == grid[0].length - 1) {
+                return cur[0];
+            }
+            
+            for (int i = 0; i < 4; i++) {
+                int nextX = x + d[i], nextY = y + d[i + 1];
+                if (0 <= nextX && nextX < grid.length && 0 <= nextY && nextY < grid[0].length && !vis[nextX][nextY]) {
+                    vis[nextX][nextY] = true;
+                    minHeap.offer(new int[]{Math.max(cur[0], grid[nextX][nextY]), nextX, nextY});
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
--- Lintcode Java/1063. My Calendar III/src/MyCalendarThree.java
+++ Lintcode Java/1063. My Calendar III/src/MyCalendarThree.java
@@ -1,6 +1,5 @@
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class MyCalendarThree {
     
