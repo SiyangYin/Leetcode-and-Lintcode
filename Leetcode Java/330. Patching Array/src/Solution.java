2a67a464e31de57738808f4602cf00423d5d8ccf
edwardwang618 <edward.wang618@gmail.com>
2021-07-24 19:15:20 -0400

    leet 330

--- Leetcode Java/330. Patching Array/src/Solution.java
+++ Leetcode Java/330. Patching Array/src/Solution.java
@@ -1,5 +1,16 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-    
+        long x = 1;
+        int i = 0, res = 0;
+        while (x <= n) {
+            if (i < nums.length && nums[i] <= x) {
+                x += nums[i++];
+            } else {
+                x += x;
+                res++;
+            }
+        }
+        
+        return res;
     }
 }
