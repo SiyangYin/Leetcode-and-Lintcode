e7ccd202731cbda3abb9ba228bbbeb1a2fd7a7ff
edwardwang618 <edward.wang618@gmail.com>
2021-07-24 22:01:04 -0400

    lint 1066

---
+++ Lintcode Java/1066. Verify Preorder Serialization of a Binary Tree/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution {
+    /**
+     * @param preorder: a string
+     * @return: return a bool
+     */
+    public boolean isValidSerialization(String preorder) {
+        // write your code here
+        String[] strs = preorder.split(",");
+        Deque<String> stk = new LinkedList<>();
+        for (int i = 0; i < strs.length; i++) {
+            String s = strs[i];
+            if (s.equals("#")) {
+                while (!stk.isEmpty() && stk.peek().equals("#")) {
+                    stk.pop();
+                    if (stk.isEmpty()) {
+                        return false;
+                    }
+                    
+                    stk.pop();
+                }
+            }
+            
+            stk.push(s);
+        }
+        
+        return stk.size() == 1 && stk.peek().equals("#");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+        System.out.println(new Solution().isValidSerialization("9,3,4,#,#,#,#"));
+    }
+}
