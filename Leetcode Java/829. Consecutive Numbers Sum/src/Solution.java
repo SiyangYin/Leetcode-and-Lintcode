43cce598d1e1a05d1d223105d63d1e0cf205cb49
edwardwang618 <edward.wang618@gmail.com>
2021-07-28 15:19:06 -0400

    leet 829

---
+++ Leetcode Java/829. Consecutive Numbers Sum/src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int consecutiveNumbersSum(int n) {
+        int res = 0;
+        for (int i = 1; i * i < 2 * n; i++) {
+            if ((n - i * (i - 1) / 2) % i == 0) {
+                res++;
+            }
+        }
+        
+        return res;
+    }
+}
