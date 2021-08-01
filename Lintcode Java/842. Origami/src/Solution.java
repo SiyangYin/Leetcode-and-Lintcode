4d54fd2c66cc05489fa43298c1ee574a15149c0a
edwardwang618 <edward.wang618@gmail.com>
2021-08-01 17:45:51 -0400

    lint 842

--- Lintcode Java/842. Origami/src/Solution.java
+++ Lintcode Java/842. Origami/src/Solution.java
@@ -5,6 +5,18 @@ public class Solution {
      */
     public String getString(int n) {
         // Write your code here
-        return "";
+        return dfs(1, n, true);
+    }
+    
+    private String dfs(int i, int n, boolean down) {
+        if (i > n) {
+            return "";
+        }
+    
+        return dfs(i + 1, n, true) + (down ? '0' : '1') + dfs(i + 1, n, false);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new Solution().getString(2));
     }
 }
