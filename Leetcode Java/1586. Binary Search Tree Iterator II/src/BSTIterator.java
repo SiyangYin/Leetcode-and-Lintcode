9c1a00745bddb8a0b7370a3fd12103811f8ab0ae
edwardwang618 <edward.wang618@gmail.com>
2021-08-01 15:40:25 -0400

    leet 1586

--- Leetcode Java/1586. Binary Search Tree Iterator II/src/BSTIterator.java
+++ Leetcode Java/1586. Binary Search Tree Iterator II/src/BSTIterator.java
@@ -1,33 +1,71 @@
-import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BSTIterator {
     
-    private Deque<TreeNode> stack, buffer;
+    private Deque<TreeNode> stk;
+    private List<TreeNode> list;
+    private int pos;
     
     public BSTIterator(TreeNode root) {
-        stack = new ArrayDeque<>();
-        buffer = new ArrayDeque<>();
+        pos = -1;
+        list = new ArrayList<>();
+        stk = new LinkedList<>();
         while (root != null) {
-            stack.push(root);
+            stk.push(root);
             root = root.left;
         }
     }
     
     public boolean hasNext() {
-        return !stack.isEmpty();
+        return !stk.isEmpty() || pos + 1 < list.size();
     }
     
     public int next() {
-        return 0;
+        pos++;
+        if (pos < list.size()) {
+            return list.get(pos).val;
+        }
+        
+        TreeNode top = stk.pop(), right = top.right;
+        list.add(top);
+        while (right != null) {
+            stk.push(right);
+            right = right.left;
+        }
+        
+        return top.val;
     }
     
     public boolean hasPrev() {
-        return false;
+        return pos > 0;
     }
     
     public int prev() {
-        return 0;
+        return list.get(--pos).val;
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(7);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(9);
+        root.right.right = new TreeNode(20);
+        BSTIterator it = new BSTIterator(root);
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.prev());
+        System.out.println(it.next());
+        System.out.println(it.hasNext());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.hasNext());
+        System.out.println(it.hasPrev());
+        System.out.println(it.prev());
+        System.out.println(it.prev());
     }
 }
 
