3d28f1558d0f171918953fc1a54d5f5046b0d708
edwardwang618 <edward.wang618@gmail.com>
2021-08-09 01:48:50 -0400

    leet 406

--- Leetcode Java/406. Queue Reconstruction by Height/src/Solution.java
+++ Leetcode Java/406. Queue Reconstruction by Height/src/Solution.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Solution {
     public int[][] reconstructQueue(int[][] people) {
-        return null;
+        PriorityQueue<int[]> queue = new PriorityQueue<>(
+                (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(b[0], a[0]));
+        
+        for (int[] cur : people) {
+            queue.offer(cur);
+        }
+        
+        List<int[]> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int[] person = queue.poll();
+            res.add(person[1], person);
+        }
+        
+        return res.toArray(new int[people.length][2]);
+    }
+    
+    public static void main(String[] args) {
+        int[][] p = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        System.out.println(Arrays.deepToString(new Solution().reconstructQueue(p)));
     }
 }
