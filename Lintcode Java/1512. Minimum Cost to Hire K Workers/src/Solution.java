f0f3a2012c24a71c8fef81dea04ca16e3fc29eb8
edwardwang618 <edward.wang618@gmail.com>
2021-09-03 11:28:44 -0400

    lint 1512

---
+++ Lintcode Java/1512. Minimum Cost to Hire K Workers/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Solution {
+    /**
+     * @param quality: an array
+     * @param wage:    an array
+     * @param K:       an integer
+     * @return: the least amount of money needed to form a paid group
+     */
+    public double mincostToHireWorkers(int[] quality, int[] wage, int K) {
+        // Write your code here
+        List<double[]> list = new ArrayList<>();
+        for (int i = 0; i < quality.length; i++) {
+            list.add(new double[]{quality[i], wage[i]});
+        }
+        list.sort((w1, w2) -> Double.compare(w1[1] / w1[0], w2[1] / w2[0]));
+        
+        double res = 1e18, sum = 0.0;
+        PriorityQueue<double[]> minHeap = new PriorityQueue<>((d1, d2) -> -Double.compare(d1[0], d2[0]));
+        for (double[] w : list) {
+            sum += w[0];
+            minHeap.offer(w);
+            if (minHeap.size() > K) {
+                sum -= minHeap.poll()[0];
+            }
+            
+            if (minHeap.size() == K) {
+                res = Math.min(res, w[1] / w[0] * sum);
+            }
+        }
+        
+        return res;
+    }
+}
