df0d97ed20888d13cb36b95d12765aea43ace8d5
edwardwang618 <edward.wang618@gmail.com>
2021-09-22 15:04:31 -0400

    leet 1894

---
+++ Leetcode Java/1894. Find the Student that Will Replace the Chalk/src/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        long sum = 0;
+        for (int x : chalk) {
+            sum += x;
+        }
+        
+        k %= sum;
+        for (int i = 0; i < chalk.length; i++) {
+            if (chalk[i] > k) {
+                return i;
+            }
+            
+            k -= chalk[i];
+        }
+        
+        return -1;
+    }
+}
