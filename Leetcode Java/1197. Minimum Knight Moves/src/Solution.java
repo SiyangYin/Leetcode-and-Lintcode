6227295869292773fef2abab53d05d2ff01cff16
edwardwang618 <edward.wang618@gmail.com>
2021-12-29 16:51:36 -0800

    leet 1197

--- Leetcode Java/1197. Minimum Knight Moves/src/Solution.java
+++ Leetcode Java/1197. Minimum Knight Moves/src/Solution.java
@@ -1,94 +1,38 @@
-import java.util.*;
-
 public class Solution {
-    class Pair {
-        int x, y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    
-        @Override
-        public boolean equals(Object o) {
-            Pair pair = (Pair) o;
-            return x == pair.x && y == pair.y;
-        }
-    
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-    
     public int minKnightMoves(int x, int y) {
         if (x == 0 && y == 0) {
             return 0;
         }
-        
+    
         x = Math.abs(x);
         y = Math.abs(y);
-        
+    
         if (x < y) {
             int tmp = x;
             x = y;
             y = tmp;
         }
-        
-        int[][] dirs = {{1, 2}, {2, 1}, {1, -2}, {-2, 1}, {-1, 2}, {2, -1}, {-1, -2}, {-2, -1}};
-        Queue<Pair> beginQueue = new LinkedList<>(), endQueue = new LinkedList<>();
-        Set<Pair> beginVisited = new HashSet<>(), endVisited = new HashSet<>();
-        Pair start = new Pair(0, 0), end = new Pair(x, y);
-        beginQueue.offer(start);
-        beginVisited.add(start);
-        endQueue.offer(end);
-        endVisited.add(end);
-        
-        int res = 0;
-        while (!beginQueue.isEmpty() && !endQueue.isEmpty()) {
-            int beginSize = beginQueue.size(), endSize = endQueue.size();
-            
-            res++;
-            for (int i = 0; i < beginSize; i++) {
-                Pair cur = beginQueue.poll();
-                for (Pair next : getNexts(cur, beginVisited, dirs, x, y)) {
-                    if (endVisited.contains(next)) {
-                        return res;
-                    }
-                    beginQueue.offer(next);
-                    beginVisited.add(next);
-                }
-            }
-            
-            res++;
-            for (int i = 0; i < endSize; i++) {
-                Pair cur = endQueue.poll();
-                for (Pair next : getNexts(cur, endVisited, dirs, x, y)) {
-                    if (beginVisited.contains(next)) {
-                        return res;
-                    }
-                    endQueue.offer(next);
-                    endVisited.add(next);
-                }
-            }
-        }
-        
-        return 0;
+    
+        return f(x, y);
     }
     
-    private Set<Pair> getNexts(Pair cur, Set<Pair> visited, int[][] dirs, int destX, int destY) {
-        Set<Pair> nexts = new HashSet<>();
-        for (int i = 0; i < dirs.length; i++) {
-            int nextX = cur.x + dirs[i][0], nextY = cur.y + dirs[i][1];
-            if (nextX < -1 || nextX > destX + 2 || nextY < -1 || nextY > destY + 2) {
-                continue;
-            }
-            
-            Pair nextP = new Pair(nextX, nextY);
-            if (!visited.contains(nextP)) {
-                nexts.add(nextP);
-            }
+    int f(int x, int y) {
+        if (x == 1 && y == 0) {
+            return 3;
         }
-        
-        return nexts;
+        if (x == 2 && y == 2) {
+            return 4;
+        }
+    
+        int delta = x - y;
+        if (y > delta) {
+            return (int) (delta - 2 * Math.floor((double) (delta - y) / 3.0));
+        } else {
+            return (int) (delta - 2 * Math.floor((double) (delta - y) / 4.0));
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new Solution().minKnightMoves(1325325 - 12, 325553 - 432));
     }
 }
--- Leetcode Java/1197. Minimum Knight Moves/src/Solution2.java
+++ Leetcode Java/1197. Minimum Knight Moves/src/Solution2.java
@@ -1,74 +1,32 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution2 {
-    class Pair {
-        int x, y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        
-        @Override
-        public boolean equals(Object o) {
-            Solution.Pair pair = (Solution.Pair) o;
-            return x == pair.x && y == pair.y;
-        }
-        
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-    
     public int minKnightMoves(int x, int y) {
-        if (x == 0 && y == 0) {
-            return 0;
-        }
-        
         x = Math.abs(x);
         y = Math.abs(y);
+        Map<String, Integer> map = new HashMap<>();
         
-        int[][] dirs = {{1, 2}, {2, 1}, {1, -2}, {-2, 1}, {-1, 2}, {2, -1}, {-1, -2}, {-2, -1}};
-        Queue<Pair> queue = new LinkedList<>();
-        Set<Pair> visited = new HashSet<>();
-        Pair start = new Pair(0, 0);
-        queue.offer(start);
-        visited.add(start);
-        
-        int res = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            
-            res++;
-            for (int i = 0; i < size; i++) {
-                Pair cur = queue.poll();
-                for (Pair next : getNexts(cur, visited, dirs)) {
-                    if (next.x == x && next.y == y) {
-                        return res;
-                    }
-                    queue.offer(next);
-                    visited.add(next);
-                }
-            }
-        }
-        
-        return 0;
+        return dfs(x, y, map);
     }
     
-    private Set<Pair> getNexts(Pair cur, Set<Pair> visited, int[][] dirs) {
-        Set<Pair> nexts = new HashSet<>();
-        for (int i = 0; i < dirs.length; i++) {
-            int nextX = cur.x + dirs[i][0], nextY = cur.y + dirs[i][1];
-            Pair nextP = new Pair(nextX, nextY);
-            if (!visited.contains(nextP) && isOptimal(nextX, nextY)) {
-                nexts.add(nextP);
-            }
+    private int dfs(int x, int y, Map<String, Integer> map) {
+        if (x + y == 0) {
+            return 0;
+        } else if (x + y == 2) {
+            return 2;
         }
         
-        return nexts;
+        String pos = x + "," + y;
+        if (map.containsKey(pos)) {
+            return map.get(pos);
+        }
+        
+        map.put(pos, 1 + Math.min(dfs(Math.abs(x - 1), Math.abs(y - 2), map), dfs(Math.abs(x - 2), Math.abs(y - 1), map)));
+        return map.get(pos);
     }
     
-    private boolean isOptimal(int x, int y) {
-        return -1 <= x && x < 301 && -1 <= y && y < 301;
+    public static void main(String[] args) {
+        System.out.println(new Solution2().minKnightMoves(2, 1));
     }
 }
