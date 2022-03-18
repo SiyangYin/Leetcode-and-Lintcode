9b50e85ef4597826a01d2624949c9a85ea648f4e
edwardwang618 <edward.wang618@gmail.com>
2022-03-17 23:10:51 -0700

    leet 1566

---
+++ Leetcode Java/1566. Detect Pattern of Length M Repeated K or More Times/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean containsPattern(int[] arr, int m, int k) {
+        for (int i = 0, j = i + m, cnt = 0; j < arr.length; ++i, ++j) {
+            if (arr[i] != arr[j]) {
+                cnt = 0;
+            } else if (++cnt == (k - 1) * m) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
