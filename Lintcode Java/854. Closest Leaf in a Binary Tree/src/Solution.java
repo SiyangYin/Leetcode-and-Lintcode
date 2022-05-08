bea3933680080ca02173cdad878249111d1359c8
edwardwang618 <edward.wang618@gmail.com>
2022-05-08 14:34:28 -0700

    lint 854

--- Lintcode Java/854. Closest Leaf in a Binary Tree/src/Solution.java
+++ Lintcode Java/854. Closest Leaf in a Binary Tree/src/Solution.java
@@ -1,71 +1,61 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Solution {
     /**
      * @param root: the root
-     * @param k: an integer
+     * @param k:    an integer
      * @return: the value of the nearest leaf node to target k in the tree
      */
     public int findClosestLeaf(TreeNode root, int k) {
         // Write your code here
-        int res = Integer.MAX_VALUE;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            TreeNode cur = queue.poll();
+        Map<TreeNode, TreeNode> par = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode nodek = null;
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
             if (cur.val == k) {
-                if (cur.left != null) {
-                    queue.clear();
-                    queue.offer(cur.left);
-                    break;
-                }
-                if (cur.right != null) {
-                    queue.clear();
-                    queue.offer(cur.right);
-                    break;
-                }
+                nodek = cur;
             }
             
-            if (cur.left == null && cur.right == null && Math.abs(cur.val - k) < Math.abs(res - k)) {
-                res = cur.val;
-            }
             if (cur.left != null) {
-                queue.offer(cur.left);
+                par.put(cur.left, cur);
+                q.offer(cur.left);
             }
             if (cur.right != null) {
-                queue.offer(cur.right);
+                par.put(cur.right, cur);
+                q.offer(cur.right);
             }
         }
         
-        if (!queue.isEmpty()) {
-            res = Integer.MAX_VALUE;
+        q.offer(nodek);
+        Set<TreeNode> set = new HashSet<>();
+        set.add(nodek);
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            if (cur == null) {
+                continue;
+            }
+            
+            if (cur.left == null && cur.right == null) {
+                return cur.val;
+            }
+            
+            if (cur.left != null && !set.contains(cur.left)) {
+                q.offer(cur.left);
+                set.add(cur.left);
+            }
+            if (cur.right != null && !set.contains(cur.right)) {
+                q.offer(cur.right);
+                set.add(cur.right);
+            }
+            if (par.containsKey(cur) && !set.contains(par.get(cur))) {
+                q.offer(par.get(cur));
+                set.add(par.get(cur));
+            }
         }
         
-        while (!queue.isEmpty()) {
-            TreeNode cur = queue.poll();
-            if (cur.left == null && cur.right == null && Math.abs(cur.val - k) < Math.abs(res - k)) {
-                res = cur.val;
-            }
-            if (cur.left != null) {
-                queue.offer(cur.left);
-            }
-            if (cur.right != null) {
-                queue.offer(cur.right);
-            }
-        }
-        
-        return res;
-    }
-    
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(3);
-        root.right = new TreeNode(4);
-        // root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(9);
-        root.right.right.right = new TreeNode(10);
-        System.out.println(new Solution().findClosestLeaf(root, 4));
+        return -1;
     }
 }
 
